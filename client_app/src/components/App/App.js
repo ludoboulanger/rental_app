36deee9126d0c5eebdae1ca37a8ff041d9fac721
Victor <vicfreve@gmail.com>
2021-05-30 22:35:11 -0400

    Apparement j'avais pas add mes changements oupsi

--- client_app/src/components/App/App.js
+++ client_app/src/components/App/App.js
@@ -1,5 +1,6 @@
 import Home from '../../pages/Home';
 import './App.css';
+import { Suspense } from 'react';
 
 function App() {
   return (
@@ -9,4 +10,11 @@ function App() {
   );
 }
 
-export default App;
+// here app catches the suspense from page in case translations are not yet loaded
+export default function WrappedApp() {
+    return (
+        <Suspense fallback="...is loading">
+            <App />
+        </Suspense>
+    );
+}
--- client_app/src/index.js
+++ client_app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './services/localization/i18n'
 import App from './components/App/App';
 
 ReactDOM.render(
