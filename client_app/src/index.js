a976b0bfa55fee5c60c46bc246f155b5ab48f905
Victor <vicfreve@gmail.com>
2021-05-30 22:42:18 -0400

    Renamed i18n js file

--- client_app/src/index.js
+++ client_app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './services/localization/i18n'
+import './services/LocalizationService'
 import App from './components/App/App';
 
 ReactDOM.render(
