9cbae8e1617c1deb0091e7014c703c0b158d7c06
ludoboulanger <ludovic.boulanger@outlook.com>
2021-06-05 14:16:10 -0400

    Fixes in PingSchema

--- backend_app/database/schemas/PingSchema.js
+++ backend_app/database/schemas/PingSchema.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Ping = new Schema(
-  {
-    body: { type: String, required: true },
-  },
-  { timestamps: true }
+	{
+		body: { type: String, required: true },
+	},
+	{ timestamps: true }
 );
 
 module.exports = mongoose.model("pings", Ping);
