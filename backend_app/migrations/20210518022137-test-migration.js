526325cce4b0a97c6c0204e59f907e8d92eb82e6
ludoboulanger <ludovic.boulanger@outlook.com>
2021-06-05 14:02:03 -0400

    Fixes for backend

--- backend_app/migrations/20210518022137-test-migration.js
+++ backend_app/migrations/20210518022137-test-migration.js
@@ -1,38 +1,40 @@
-'use strict';
+"use strict";
 
-var dbm;
+let dbm;
+// eslint-disable-next-line no-unused-vars
 var type;
+// eslint-disable-next-line no-unused-vars
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
-exports.setup = function(options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
+	* We receive the dbmigrate dependency from dbmigrate initially.
+	* This enables us to not have to rely on NODE_PATH.
+	*/
+exports.setup = function (options, seedLink) {
+	dbm = options.dbmigrate;
+	type = dbm.dataType;
+	seed = seedLink;
 };
 
-exports.up = function(db) {
-  return db.insert("testCollection", [{
-    name: 'Ludo',
-    skill: 'Express.js'
-  },
-    {
-      name: 'Mauri',
-      skill: 'Soif d\'apprentissage'
-    },
-    {
-      name: 'Vic',
-      skill: 'Joli sourire'
-    }]);
+exports.up = function (db) {
+	return db.insert("testCollection", [{
+		name: "Ludo",
+		skill: "Express.js"
+	},
+	{
+		name: "Mauri",
+		skill: "Soif d'apprentissage"
+	},
+	{
+		name: "Vic",
+		skill: "Joli sourire"
+	}]);
 };
 
-exports.down = function(db) {
-  return db.dropCollection("testCollection");
+exports.down = function (db) {
+	return db.dropCollection("testCollection");
 };
 
 exports._meta = {
-  "version": 1
+	"version": 1
 };
--- backend_app/server/index.js
+++ backend_app/server/index.js
@@ -1,20 +1,20 @@
-require('dotenv-safe').config();
-const express = require('express');
-const Database = require('../database/index');
-const cors = require('cors')
+require("dotenv-safe").config();
+const express = require("express");
+const Database = require("../database/index");
+const cors = require("cors");
 const PORT = 8000;
 
-Database.on('error', console.error.bind(console, 'MongoDB connection error:'));
+Database.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.status(200).send("Hello World!");
+app.get("/", (req, res) => {
+	res.status(200).send("Hello World!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`);
+	console.log(`Server listening on PORT ${PORT}`);
 });
