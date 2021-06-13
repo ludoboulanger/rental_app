cf20cd9eeaca865ca30d8a84cdf47ce3e4b0a866
ludoboulanger <ludovic.boulanger@outlook.com>
2021-06-13 17:35:08 -0400

    Change to mongodb

--- backend_app/database/index.js
+++ backend_app/database/index.js
@@ -1,11 +1,30 @@
-const mongoose = require("mongoose");
-console.log(process.env.DEV_DB_NAME);
-mongoose
-  .connect("mongodb://127.0.0.1:27017/" + process.env.DEV_DB_NAME, { useNewUrlParser: true,  useUnifiedTopology: true  })
-  .catch((e) => {
-    console.error("Connection error", e.message);
-  });
+const { MongoClient } = require("mongodb");
+const DB_NAME = process.env.DEV_DB_NAME;
+const DB_URI = `mongodb://127.0.0.1:27017/${DB_NAME}`;
 
-const Database = mongoose.connection;
+// Create a new MongoClient
+const CLIENT = new MongoClient(DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-module.exports = Database;
+const initMongoConnection = async () => {
+  try {
+    await CLIENT.connect();
+  } catch (e) {
+    console.error("Error Connecting to Database");
+  }
+};
+
+const closeMongoConnection = async () => {
+  try {
+    await CLIENT.close();
+  } catch (e) {
+    console.error("Error closing connection to Database");
+  }
+};
+
+module.exports = {
+  initMongoConnection,
+  closeMongoConnection,
+};
