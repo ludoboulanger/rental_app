9d81b3ee879326e3e1d0a9c977e38444763abd6b
ludoboulanger <ludovic.boulanger@outlook.com>
2021-06-13 20:06:44 -0400

    Cleanups

--- backend_app/api/routes/Authentication.js
+++ backend_app/api/routes/Authentication.js
@@ -1,10 +1,7 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
 const { ajv } = require("../schemas/schemas");
-const { v4: uuidv4 } = require("uuid");
-const { invokeAndSafelyClose } = require("../../services/MongoService");
+const { checkIfEmailExists, insertNewUser } = require("../utils/Persistence");
 
-const SALT_ROUNDS = 10;
 const AuthenticationRouter = express.Router();
 
 AuthenticationRouter.post("/signup", async (request, response, next) => {
@@ -16,47 +13,18 @@ AuthenticationRouter.post("/signup", async (request, response, next) => {
     return;
   }
 
-  const [foundEmail, errorCheckingEmail] = await invokeAndSafelyClose(
-    async (client) =>
-      client
-        .db(process.env.DEV_DB_NAME)
-        .collection("users")
-        .findOne({ email: body.email })
-  );
+  const doesEmailExist = await checkIfEmailExists(body.email);
 
-  if (errorCheckingEmail) {
-    next("500");
-    return;
-  }
-
-  if (foundEmail) {
+  if (doesEmailExist) {
     next("403");
     return;
   }
 
-  try {
-    body.password = await bcrypt.hash(body.password, SALT_ROUNDS);
-    body._id = uuidv4();
-    body.status = "pending";
-    body.activationCode = "";
+  const created = insertNewUser(body);
 
-    const [created, errorCreatingUser] = await invokeAndSafelyClose(
-      async (client) =>
-        client.db(process.env.DEV_DB_NAME).collection("users").insertOne(body)
-    );
-
-    if (errorCreatingUser || created.result.ok !== 1) {
-      next("500");
-      return;
-    }
-
-    response
-      .status(201)
-      .send({ message: "User Created Successfully", id: created.insertedId });
-  } catch (e) {
-    next("500");
-    return;
-  }
+  response
+    .status(201)
+    .send({ message: "User Created Successfully", id: created.insertedId });
 });
 
 module.exports = AuthenticationRouter;
