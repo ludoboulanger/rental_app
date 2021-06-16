dcee31bcbaadacb641b1cf15ab8147a6126ef4e2
ludoboulanger <ludovic.boulanger@outlook.com>
2021-06-15 20:21:02 -0400

    Automated tests pass

--- backend_app/api/tests/routes.spec.js
+++ backend_app/api/tests/routes.spec.js
@@ -2,7 +2,7 @@ const sinon = require("sinon");
 const expect = require("chai").expect;
 const app = require("../index");
 const request = require("supertest");
-const UserPersistence = require("../utils/UserPersistence");
+const AccountInfo = require("../../mongo/AccountInfo");
 const { userCreatedStub } = require("./DatabaseMock");
 const { describe, it, beforeEach, afterEach } = require("mocha");
 
@@ -10,51 +10,35 @@ describe("Server Routes Tests", () => {
   describe("User Authentication Routes", () => {
     describe("User Sign up", () => {
       let createUserStub;
-      let checkIfUserExistsStub;
 
       beforeEach(() => {
         createUserStub = sinon
-          .stub(UserPersistence, "insertNewUser")
+          .stub(AccountInfo, "createNewAccountInfo")
           .returns(userCreatedStub);
-
-        checkIfUserExistsStub = sinon.stub(
-          UserPersistence,
-          "checkIfEmailExists"
-        );
       });
 
       afterEach(() => {
         createUserStub.restore();
-        checkIfUserExistsStub.restore();
       });
 
       it("Returns 400 on missing data fields", async () => {
         const dataMissingFirstname = {
           lastname: "Stark",
           email: "stark@avengers.com",
-          password: "Password1!",
         };
 
         const dataMissingLastname = {
           firstname: "Tony",
           email: "stark@avengers.com",
-          password: "Password1!",
         };
 
         const dataMissingEmail = {
           firstname: "Tony",
           lastname: "Stark",
-          password: "Password1!",
-        };
-
-        const dataMissingPassword = {
-          firstname: "Tony",
-          lastname: "Stark",
-          email: "stark@avengers.com",
         };
 
         await request(app)
-          .post("/api/users/signup")
+          .post("/api/users/create-account")
           .send(dataMissingFirstname)
           .expect(400)
           .then((res) => {
@@ -62,7 +46,7 @@ describe("Server Routes Tests", () => {
           });
 
         await request(app)
-          .post("/api/users/signup")
+          .post("/api/users/create-account")
           .send(dataMissingLastname)
           .expect(400)
           .then((res) => {
@@ -70,16 +54,8 @@ describe("Server Routes Tests", () => {
           });
 
         await request(app)
-          .post("/api/users/signup")
+          .post("/api/users/create-account")
           .send(dataMissingEmail)
-          .expect(400)
-          .then((res) => {
-            expect(res.body.message).to.be.eql("Invalid Request");
-          });
-
-        await request(app)
-          .post("/api/users/signup")
-          .send(dataMissingPassword)
           .expect(400)
           .then((res) => {
             expect(res.body.message).to.be.eql("Invalid Request");
@@ -91,33 +67,22 @@ describe("Server Routes Tests", () => {
           firstname: 23434,
           lastname: "Stark",
           email: "stark@avengers.com",
-          password: "Password1!",
         };
 
         const invalidLastname = {
           firstname: "Tony",
           lastname: 3255435345,
           email: "stark@avengers.com",
-          password: "Password1!",
         };
 
         const invalidEmail = {
           firstname: "Tony",
           lastname: "Stark",
           email: "starkavengerscom",
-          password: "Password1!",
-        };
-
-        const invalidPassword = {
-          firstname: "Tony",
-          lastname: "stark",
-          email: "stark@avengers.com",
-          password:
-            "fnjkdsl bverkFDLGM;KLFDGKDFHKMGDFKLHMFGKLHMGFKHMKFGHKGFKHKLGFHLKGFMehtrgjkdfgh j;iuretiorjghiurehti rjguer'waekpfao]rwOPEJTOIJRIOTEJRPOGHREUIHYORERJOIRHGROIEKRO REITEIJEWHTUQTEWHUIEH7 3Y578B34635U79805U69 6895YU-69",
         };
 
         await request(app)
-          .post("/api/users/signup")
+          .post("/api/users/create-account")
           .send(invalidFirstname)
           .expect(400)
           .then((res) => {
@@ -125,7 +90,7 @@ describe("Server Routes Tests", () => {
           });
 
         await request(app)
-          .post("/api/users/signup")
+          .post("/api/users/create-account")
           .send(invalidLastname)
           .expect(400)
           .then((res) => {
@@ -133,80 +98,45 @@ describe("Server Routes Tests", () => {
           });
 
         await request(app)
-          .post("/api/users/signup")
+          .post("/api/users/create-account")
           .send(invalidEmail)
           .expect(400)
           .then((res) => {
             expect(res.body.message).to.be.eql("Invalid Request");
           });
-
-        await request(app)
-          .post("/api/users/signup")
-          .send(invalidPassword)
-          .expect(400)
-          .then((res) => {
-            expect(res.body.message).to.be.eql("Invalid Request");
-          });
-      });
-
-      it("Returns 403 if user already exists in the database", async () => {
-        checkIfUserExistsStub.returns(true);
-        const user = {
-          firstname: "Tony",
-          lastname: "Stark",
-          email: "stark@avengers.com",
-          password: "Password1!",
-        };
-
-        await request(app)
-          .post("/api/users/signup")
-          .send(user)
-          .expect(403)
-          .then((res) => {
-            expect(res.body.message).to.eql("Action Forbidden");
-            expect(checkIfUserExistsStub.calledOnce).to.be.true;
-          });
       });
 
       it("Returns 201 on successful creation", async () => {
-        checkIfUserExistsStub.returns(false);
-
         const user = {
           firstname: "Tony",
           lastname: "Stark",
           email: "stark@avengers.com",
-          password: "Password1!",
         };
 
         await request(app)
-          .post("/api/users/signup")
+          .post("/api/users/create-account")
           .send(user)
           .expect(201)
           .then(() => {
-            expect(checkIfUserExistsStub.calledOnce).to.be.true;
             expect(createUserStub.calledOnce).to.be.true;
           });
       });
 
       it("Returns the id of the new user on successful creation", async () => {
-        checkIfUserExistsStub.returns(false);
-
         const user = {
           firstname: "Tony",
           lastname: "Stark",
           email: "stark@avengers.com",
-          password: "Password1!",
         };
 
         await request(app)
-          .post("/api/users/signup")
+          .post("/api/users/create-account")
           .send(user)
           .expect(201)
           .then((res) => {
             let resBody = res.body;
-            expect(resBody.message).to.eql("User Created Successfully");
+            expect(resBody.message).to.eql("Account Created Successfully");
             expect(resBody.id).to.eql("9eb7bdc3-fb64-41fe-83da-052febf8b4dd");
-            expect(checkIfUserExistsStub.calledOnce).to.be.true;
             expect(createUserStub.calledOnce).to.be.true;
           });
       });
