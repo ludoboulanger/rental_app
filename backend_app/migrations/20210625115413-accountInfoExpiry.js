55549041803bee9c64433e3783a8dbe03f83d48e
Ludovic Boulanger <ludo.boulanger@gmail.com>
2021-06-26 18:07:28 -0400

    Fixes 3

--- backend_app/migrations/20210625115413-accountInfoExpiry.js
+++ backend_app/migrations/20210625115413-accountInfoExpiry.js
@@ -1,27 +1,9 @@
-const { invokeAndSafelyClose } = require("../mongo/Connection");
-
 module.exports = {
-  async up() {
-    const [result, error] = await invokeAndSafelyClose(
-      client => client.db(process.env.DB_NAME).collection("accountInfo").createIndex({lastModified: 1}, {expireAfterSeconds: 600}) // Account expires after 10mins
-    );
-
-    if (error) {
-      throw "Cant migrate up. Error creating accountInfoExpiry index";
-    }
-
-    return result;
+  async up(db, client) {
+    await client.db(process.env.DB_NAME).collection("accountInfo").createIndex({lastModified: 1}, {expireAfterSeconds: 600});
   },
 
-  async down() {
-    const [result, error] = await invokeAndSafelyClose(
-      client => client.db(process.env.DB_NAME).collection("accountInfo").dropIndex("lastModified_1")
-    );
-
-    if (error) {
-      throw "Cant migrate down. Error dropping accountInfoExpiry index";
-    }
-
-    return result;
+  async down(db, client) {
+    await client.db(process.env.DB_NAME).collection("accountInfo").dropIndex("lastModified_1");
   }
 };
