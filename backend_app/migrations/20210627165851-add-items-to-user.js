a13310ca16663178359dd015335fe2c8475835dc
Victor <vicfreve@gmail.com>
2021-06-27 14:22:08 -0400

    Added listings array to user

--- backend_app/migrations/20210627165851-add-items-to-user.js
+++ backend_app/migrations/20210627165851-add-items-to-user.js
@@ -1,9 +1,25 @@
 module.exports = {
   async up(db) {
 
+    let validator = (await db.listCollections({name: "user"}).toArray())[0].options.validator;
+
+    validator.$jsonSchema.properties.listings = {bsonType:"array", items: {bsonType: "objectId"}};
+
+    await db.command({
+      collMod: "user",
+      validator: validator,
+    });
+
   },
 
   async down(db) {
+    let validator = (await db.listCollections({name: "user"}).toArray())[0].options.validator;
 
+    delete validator.$jsonSchema.properties.listings;
+
+    await db.command({
+      collMod: "user",
+      validator: validator,
+    });
   }
 };
