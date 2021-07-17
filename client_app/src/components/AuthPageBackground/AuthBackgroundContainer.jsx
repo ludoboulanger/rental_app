680ffc6a057938aa2c7ea76e890693284f99d795
Ludovic Boulanger <ludo.boulanger@gmail.com>
2021-07-17 11:12:34 -0400

    Fixed Back button

--- client_app/src/components/AuthPageBackground/AuthBackgroundContainer.jsx
+++ client_app/src/components/AuthPageBackground/AuthBackgroundContainer.jsx
@@ -22,7 +22,7 @@ export default function AuthBackgroundContainer({ children }) {
   const history = useHistory();
 
   const handleBackClick = () => {
-    history.back();
+    history.goBack();
   };
 
   return (
