2b47fdf1c1206985efcf6fc07659545295908b45
Ludovic Boulanger <ludo.boulanger@gmail.com>
2021-07-18 18:03:16 -0400

    WIP

---
+++ client_app/src/pages/VerificationPage/VerificationPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  Container,
+  Grid,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import { useFormik } from "formik";
+import { useTranslations } from "i18next";
+import OtpInput from "react-otp-input";
+
+export default function VerificationPage() {
+
+}
